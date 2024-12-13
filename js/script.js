243f11eb17c7ce91da62472f601042e4d909e0a4
YourName <gamoctobre@gmail.com>
2024-12-13 20:32:23 +0800

    git

---
+++ js/script.js
@@ -0,0 +1,23 @@
+fetch('https://official-joke-api.appspot.com/random_joke', {method: 'GET',})
+      .then(x => x.json())
+      .then(y => myDisplay(y))
+      .catch(error => {
+        alert('error man ka');
+      });
+
+    function myDisplay(y){
+      let jokeDis = document.getElementById('joke-dis');
+      let jokeAns = document.getElementById('joke-answer');
+      let button1 = document.getElementById('btn-ans');
+      let button2 = document.getElementById('btn-ref');
+
+    jokeDis.innerHTML = y.setup;
+    jokeAns.innerHTML = y.punchline;
+    
+    button1.onclick = function(){
+      jokeAns.style.display = "block";
+        }
+             button2.onclick = function(){
+             location.reload();
+        }
+    }
