db03ef00b67d14d7b996389502dd1ee2fcc7103f
YourName <gamoctobre@gmail.com>
2024-12-21 23:28:03 +0800

    ll

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,8 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
-
-if (workbox) {
-  console.log('Workbox loaded successfully');
-
+importScripts(
+    'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+  );
   
   workbox.routing.registerRoute(
-    ({ request }) => request.destination === 'image',
-    new workbox.strategies.CacheFirst({
-      cacheName: 'icon-cache',
-      plugins: [
-        new workbox.expiration.ExpirationPlugin({
-          maxEntries: 10,
-          maxAgeSeconds: 7 * 24 * 60 * 60,
-        }),
-      ],
-    })
+    ({request}) => request.destination === 'image',
+    new workbox.strategies.CacheFirst()
   );
-}
