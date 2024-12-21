5c7d1e4a1d854d72e5742e7b717e73b4e5ff1da3
YourName <gamoctobre@gmail.com>
2024-12-21 22:04:42 +0800

    h

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,22 @@
-importScripts(
-    'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
-  );
-  
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+
+if (workbox) {
+  console.log('Workbox loaded successfully');
   workbox.routing.registerRoute(
-    ({request}) => request.destination === 'image',
-    new workbox.strategies.CacheFirst()
+    ({ request }) => request.destination === 'image',
+    
+    new workbox.strategies.CacheFirst({
+      cacheName: 'image-cache',
+      plugins: [
+        new workbox.expiration.ExpirationPlugin({
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60, 
+        }),
+      ],
+    })
   );
+
+  workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+} else {
+  console.error('Workbox failed to load');
+}
