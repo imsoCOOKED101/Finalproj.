149d4e3aea446b12847809a24cb445fef5f45082
YourName <gamoctobre@gmail.com>
2024-12-21 22:15:52 +0800

    ws

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,14 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
-
-if (workbox) {
-  console.log('Workbox loaded successfully');
-  workbox.routing.registerRoute(
-    ({ request }) => request.destination === 'image',
-    new workbox.strategies.CacheFirst({
-      cacheName: 'image-cache',
-      plugins: [
-        new workbox.expiration.ExpirationPlugin({
-          maxEntries: 50, 
-          maxAgeSeconds: 30 * 24 * 60 * 60, 
-        }),
-      ],
+importScripts(
+    'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+  );
+  
+  const {registerRoute} = workbox.routing;
+  const {CacheFirst} = workbox.strategies;
+  const {CacheableResponse} = workbox.cacheableResponse;
+  
+  registerRoute(
+    ({request}) => request.destination === 'image',
+    new CacheFirst({
+      plugins: [new CacheableResponsePlugin({statuses: [0, 200]})],
     })
   );
-
-  workbox.routing.registerRoute(
-    ({ request }) => request.url.includes('/download'),
-    new workbox.strategies.NetworkFirst({
-      cacheName: 'download-cache',
-      plugins: [
-        new workbox.expiration.ExpirationPlugin({
-          maxEntries: 20, 
-          maxAgeSeconds: 7 * 24 * 60 * 60, 
-        }),
-      ],
-    })
-  );
-} else {
-  console.error('Workbox failed to load');
-}
-
