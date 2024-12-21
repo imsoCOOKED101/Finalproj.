494b9e648f1c0dbf7c9a7d3f363b8b593e768c1d
YourName <gamoctobre@gmail.com>
2024-12-21 22:26:00 +0800

    sw

--- js/script.js
+++ js/script.js
@@ -27,3 +27,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
   fetchJoke();
 });
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(registration => {
+          console.log('Service Worker registered:', registration);
+        })
+        .catch(error => {
+          console.error('Service Worker registration failed:', error);
+        });
+    });
+  }
+  
