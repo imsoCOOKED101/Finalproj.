31241e660a2c0a9ea86c4661682ed54e2efd1f0f
YourName <gamoctobre@gmail.com>
2024-12-21 22:32:24 +0800

    ss

--- service-worker.js
+++ service-worker.js
@@ -3,14 +3,14 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox
 if (workbox) {
   console.log('Workbox loaded successfully');
 
-
+  
   workbox.routing.registerRoute(
     ({ request }) => request.destination === 'image',
     new workbox.strategies.CacheFirst({
-      cacheName: 'image-cache',
+      cacheName: 'image-cache', 
       plugins: [
         new workbox.expiration.ExpirationPlugin({
-          maxEntries: 50, 
+          maxEntries: 50,
           maxAgeSeconds: 30 * 24 * 60 * 60, 
         }),
       ],
