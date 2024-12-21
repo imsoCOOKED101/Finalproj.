986ce214ca811bbc61d0961a25975102ce537aa1
YourName <gamoctobre@gmail.com>
2024-12-21 22:43:50 +0800

    asha

--- service-worker.js
+++ service-worker.js
@@ -7,15 +7,13 @@ if (workbox) {
   workbox.routing.registerRoute(
     ({ request }) => request.destination === 'image',
     new workbox.strategies.CacheFirst({
-      cacheName: 'image-cache', 
+      cacheName: 'icon-cache',
       plugins: [
         new workbox.expiration.ExpirationPlugin({
-          maxEntries: 50,
-          maxAgeSeconds: 30 * 24 * 60 * 60, 
+          maxEntries: 10,
+          maxAgeSeconds: 7 * 24 * 60 * 60,
         }),
       ],
     })
   );
-} else {
-  console.error('Workbox failed to load');
 }
