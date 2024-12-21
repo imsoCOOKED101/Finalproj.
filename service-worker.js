77637b134cbe1d871258a4fdc1ec11bddc65f3a0
YourName <gamoctobre@gmail.com>
2024-12-21 22:11:40 +0800

    sd

--- service-worker.js
+++ service-worker.js
@@ -4,19 +4,30 @@ if (workbox) {
   console.log('Workbox loaded successfully');
   workbox.routing.registerRoute(
     ({ request }) => request.destination === 'image',
-    
     new workbox.strategies.CacheFirst({
       cacheName: 'image-cache',
       plugins: [
         new workbox.expiration.ExpirationPlugin({
-          maxEntries: 50,
+          maxEntries: 50, 
           maxAgeSeconds: 30 * 24 * 60 * 60, 
         }),
       ],
     })
   );
 
-  workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+  workbox.routing.registerRoute(
+    ({ request }) => request.url.includes('/download'),
+    new workbox.strategies.NetworkFirst({
+      cacheName: 'download-cache',
+      plugins: [
+        new workbox.expiration.ExpirationPlugin({
+          maxEntries: 20, 
+          maxAgeSeconds: 7 * 24 * 60 * 60, 
+        }),
+      ],
+    })
+  );
 } else {
   console.error('Workbox failed to load');
 }
+
